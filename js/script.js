8e95c25c8f46ae04aeb7bfa9ff469e50a6efaa55
Alpha Dzingina <alphaadx6@gmail.com>
2017-07-30 02:45:47 +0100

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+
+    $(".page-scroll").click(function(event){     
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+    });
+
+    
+
+});
