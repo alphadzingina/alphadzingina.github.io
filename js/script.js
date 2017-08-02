a47f62a2bbec02fc3aa289b5dfc19c8856c70e1d
Alpha Dzingina <alphaadx6@gmail.com>
2017-08-02 19:33:15 +0100

    Finally! Isotope plugin for portfolio section now working! Yay!

--- js/script.js
+++ js/script.js
@@ -6,12 +6,25 @@ jQuery(document).ready(function($) {
     });
 
 
-    //isotope initialisation
-    $('.portfolio-items').isotope({
-  		// options
-  		itemSelector: '.grid-item',
-  		layoutMode: 'fitRows'
-	});
+   
+    	// cache container
+    	var $container = $('.portfolio-items');
+
+    	// initialize isotope
+    	$container.isotope({
+    		// options...
+    		animationEngine: 'best-available',
+    		itemSelector: '.portfolio-item'
+    	});
+
+    	// filter items when filter link is clicked
+    	$('.portfolio-filter li button').on('click', function() {
+    		var selector = $(this).data('filter');
+    		$container.isotope({
+    			filter: selector
+    		});
+		}); 
+
 
 
     
